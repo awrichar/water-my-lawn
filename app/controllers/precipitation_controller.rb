5ea0b6a6b356f05efdb59a318723058dc2e79e03
Andrew Richardson <awrichardson6@gmail.com>
2015-06-25 15:34:22 +0000

    Calculate lawn saturation

--- app/controllers/precipitation_controller.rb
+++ app/controllers/precipitation_controller.rb
@@ -5,6 +5,10 @@ CITY = "Durham"
 STATE = "NC"
 
 FORECAST_CACHE_TIME = 12.hours
+MAX_SATURATION = 0.5
+MIN_SATURATION = MAX_SATURATION * 0.2
+EVAPORATION_DAYS = 5
+EVAPORATION_PER_DAY = 1.0 / EVAPORATION_DAYS
 
 class PrecipitationController < ApplicationController
   def index
@@ -20,15 +24,69 @@ class PrecipitationController < ApplicationController
     # Get the current date and 4 days on each side
     wunderground = Wunderground.new(loc)
     @today = wunderground.today
-    date = @today - 4.days
+    start_date = @today - 4.days
     end_date = @today + 4.days
 
+    # Get the precipitation on each day
     @days = []
+    date = start_date
     while date <= end_date do
       precip = wunderground.get_precipitation(date)
       @days.push({ date: date, precipitation: sprintf('%0.2f', precip) })
       date += 1.days
     end
+
+    date = @today - 1.days
+    saturation_factor = 1 - EVAPORATION_PER_DAY
+    @saturation = 0
+    @last_rain = nil
+
+    # Find the last rain and the current lawn saturation
+    while @saturation < MAX_SATURATION and saturation_factor.round(3) > 0 do
+      precip = wunderground.get_precipitation(date)
+
+      if precip > 0
+        @last_rain = {date: date, precipitation: format_precip(precip)}
+      end
+
+      @saturation += [precip, MAX_SATURATION].min * saturation_factor
+
+      date -= 1.days
+      saturation_factor -= EVAPORATION_PER_DAY
+    end
+
+    date = @today
+    @next_rain = nil
+
+    # Find the next rain
+    while not @next_rain do
+      precip = wunderground.get_precipitation(date)
+      if not precip
+        break
+      elsif precip > 0
+        @next_rain = {date: date, precipitation: precip}
+      end
+    end
+
+    # Interpret the results
+    if @saturation > MIN_SATURATION
+      @status = "No"
+      @long_status = "You've had plenty of rain."
+    elsif @next_rain[:date] == @today
+      if @next_rain[:precipitation] > MIN_SATURATION
+        @status = "Don't bother"
+        @long_status = "Looks like rain today."
+      else
+        @status = "Probably"
+        @long_status = "There's rain in the forecast, but not much."
+      end
+    else
+      @status = "Yes"
+      @long_status = "Your lawn is looking a bit dry."
+    end
+
+    @saturation = format_precip(@saturation)
+    @EVAPORATION_DAYS = EVAPORATION_DAYS
   end
 
   private
@@ -114,12 +172,12 @@ class Wunderground
 
     def get_precipitation_forecast(date)
       forecast = get_forecast(date)
-      forecast['qpf_allday']['in']
+      forecast['qpf_allday']['in'].to_f
     end
 
     def get_precipitation_historical(date)
       fmt_date = date.strftime("%Y%m%d")
       resp = request("history_#{fmt_date}/q/#{@state}/#{@city}")
-      resp['history']['dailysummary'][0]['precipi']
+      resp['history']['dailysummary'][0]['precipi'].to_f
     end
 end
