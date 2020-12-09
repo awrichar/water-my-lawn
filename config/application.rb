fd09f7f17e21557c0680ab57d30f8253372152ae
Andrew Richardson <awrichardson6@gmail.com>
2020-12-09 16:10:04 -0500

    Remove API key from version control
    Oops. Purged this API key and generated a new one to be delivered in a
    secure manner.

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,21 @@ module Workspace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Set local environment variables from a file /config/local_env.yml
+    # See http://railsapps.github.com/rails-environment-variables.html
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Configure Google API key for geocoding
-    Geokit::Geocoders::GoogleGeocoder.api_key = 'AIzaSyCWgNHcySea8KAwwPsFLfuWDN6mJZgTs2c'
+    Geokit::Geocoders::GoogleGeocoder.api_key = ENV["GOOGLE_API_KEY"]
 
     # Configure Google API key for timezones
     Timezone::Configure.begin do |c|
-      c.google_api_key = 'AIzaSyCWgNHcySea8KAwwPsFLfuWDN6mJZgTs2c'
+      c.google_api_key = ENV["GOOGLE_API_KEY"]
     end
   end
 end
